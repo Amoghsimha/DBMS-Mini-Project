0bb9f797cfd8ee1531f9ea71c991f81ea89ec6c9
AMOGH SIMHA <amoghsimha@gmail.com>
2026-04-05 19:05:54 +0530

    changes

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="E:\proj\stylesheet\login1.css">
+<title>Retail Store Management System</title>
+<style>
+.hero-image {
+  background-image: url("1.jpg");
+  background-color: #cccccc;
+  height: 500px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative	;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: White;
+}
+
+</style>
+<body>
+<center>
+	<h1><b><u> Retail Store Management System</h1>
+</center>	
+<div class="hero-image">
+  <div class="hero-text">
+
+    <h2 style="color:blue;font-size:40px"><u><b>Login </h2>
+    <a href="admin/dlogin.php"> <button style="font-size:25px ;background-color:#BB8FCE">Admin Login</button></a>
+    <a href="slogin.php"> <button style="font-size:25px ;background-color:#BB8FCE">Store Login</button></a>
+    <a href="dist/dlogin.php"> <button style="font-size:25px ;background-color:#BB8FCE">Distributor Login</button></a>
+
+</div>
+</div>
+</body>
+</html>
